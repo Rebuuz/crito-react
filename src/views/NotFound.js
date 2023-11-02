0f434ff009b9f14a0d0924e346a4f882d2a58156
Rebecca <rebeccalagerr@outlook.com>
2023-11-03 00:03:33 +0100

    klar med 404 sidan

--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -3,22 +3,21 @@ import '../stylesheets/Error.css';
 import '../stylesheets/General.css';
 
 import Image from '../assets/images/404_image.png';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
 import BottomFooter from '../components/Footer/BottomFooter';
 
 const NotFound = () => {
   return (
     <div className="wrapper">
-        <Header />
         <div className="section-title">
             <h1>404</h1>
             <p>The page you're looking for doesn't seem to exist... please go back to a previous one.</p>
         </div>
+        <div className="button">
+          <a className="btn-yellow" href="#">Go Back <i className="fa-regular fa-arrow-up-right"></i></a>
+        </div>
         <div className="image">
             <img src={ Image } alt="Image of a quizzical cat" />
         </div>
-        <Footer />
         <BottomFooter />
     </div>
   )
